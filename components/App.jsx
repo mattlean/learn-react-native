7d49c266abc782c69080070d6541b79cf8d0aad5
Matthew Lean <matt@mattlean.com>
2024-12-03 16:48:01 -0800

    Render MenuItems with FlatList

--- components/App.jsx
+++ components/App.jsx
@@ -4,17 +4,17 @@ import { View } from 'react-native';
  // Import a local component here
  import LittleLemonHeader from './LittleLemonHeader';
  import LittleLemonFooter from './LittleLemonFooter';
- import WelcomeScreen from './WelcomeScreen'
- import styles from './style'
-//  import MenuItems from './MenuItems'
+//  import WelcomeScreen from './WelcomeScreen';
+ import styles from './style';
+ import MenuItems from './MenuItems';
  
  export default function App() {
   return (
     <>
       <View style={styles.headerContainer}>
         <LittleLemonHeader />
-        <WelcomeScreen />
-        {/* <MenuItems /> */}
+        {/* <WelcomeScreen /> */}
+        <MenuItems />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
